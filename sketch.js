2ca812c2e7e79502126f5fdd93ea8a16a36eb8d4
pavanpuneeth <71397053+pavanpuneeth@users.noreply.github.com>
2020-10-28 20:10:48 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -30,12 +30,7 @@ var slingshot;
 var bg;
 
 function preload() 
-  {
-   // polygonImg = loadImage("hexagon.png");
-    //bg = loadImage("star.jpg");
-
-
-  }
+{}
 
 function setup() {
   createCanvas(900,400);
@@ -54,41 +49,40 @@ function setup() {
      block4 = new Box(325,325,50,50);
      block5 = new Box(375,325,50,50);
      block6 = new Box(425,325,50,50);
-    //ABOVE BLOCKS ARE THE BASE 6 BLOCKS ON PLATFORM 1
+   
 
      block7 = new Box(225,275,50,50);
      block8 = new Box(275,275,50,50);
      block9 = new Box(325,275,50,50);
      block10 = new Box(375,275,50,50);
-    //ABOVE BLOCKS ARE THE FOUR BLOCKS ON PLATFORM 1
+   
 
      block11 = new Box(275,225,50,50);
      block12 = new Box(325,225,50,50);
-    //ABOVE BLOCKS ARE THE TWO BLOCKS ON PLATFORM 1
+    
 
      block13 = new Box(300,175,50,50);
-     //ABOVE BLOCK IS THE SINGLE BLOCK ON PLATFORM 1
+     
      
      block14 = new Box(575,250,50,50);
      block15 = new Box(625,250,50,50);
      block16 = new Box(675,250,50,50);
      block17 = new Box(725,250,50,50);
-     //ABOVE BLOCKS ARE THE FOUR BASE BLOCKS ON PLATFORM 2
+    
      
      block18 = new Box(625,200,50,50);
      block19 = new Box(675,200,50,50);
-     //ABOVE BLOCKS ARE THE TWO BLOCKS ON PLATFORM 2
+     
 
      block20 = new Box(650,150,50,50);
-     //ABOVE BLOCK IS THE SINGLE BLOCK ON PLATFORM 2
+     
 
     polygon = new Polygon;
 
     slingshot = new SlingShot(polygon.body,{x:100, y:100});
     
 
-  // console.log(polygon.position.x);
-  // console.warn(polygon.position.y);
+  
     Engine.run(engine);
 }
 
@@ -123,12 +117,7 @@ function draw() {
   polygon.display();
   slingshot.display();
 
- //fill("white");
-  //stroke("black");
-  //textSize(20)
-  //textFont("Algerian");
-  //text("Drag the Hexagon and Release it to destroy the arrangement of blocks",10,20);
-  //text("Press Space To get another Chance to Play",400,380);
+
   
   drawSprites();
 }
